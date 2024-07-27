ce6b7b225205d5569896e0b2651e6eafd41e0131
sarvagyapatel <103870100+sarvagyapatel@users.noreply.github.com>
2024-07-28 01:44:12 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,20 @@
-
+import { FaLinkedin } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa"
+import { FaSquareXTwitter } from "react-icons/fa6"
+import sarvagyaPatelLogo  from "../assets/sarvagyaPatelLogo.png"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="flex items-center justify-between py-6 mb-20">
+        <div className="flex flex-shrink-0 items-center ">
+            <img className="w-10 mx-2" src={sarvagyaPatelLogo} alt="Logo" />
+        </div>
+        <div className="flex items-center justify-between gap-4 m-8 text-2xl text-white">
+            <FaLinkedin />
+            <FaGithub />
+            <FaSquareXTwitter />
+        </div>
+    </nav>
   )
 }
 
