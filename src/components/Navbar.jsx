9d0379685e6c7162128dc2e83196cc7c11b5280c
sarvagyapatel <103870100+sarvagyapatel@users.noreply.github.com>
2024-07-28 13:25:58 +0530

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,12 @@ function Navbar() {
   return (
     <nav className="flex items-center justify-between py-6 mb-20">
         <div className="flex flex-shrink-0 items-center ">
-            <img className="w-10 mx-2" src={sarvagyaPatelLogo} alt="Logo" />
+            <img className="w-24 mx-2" src={sarvagyaPatelLogo} alt="Logo" />
         </div>
         <div className="flex items-center justify-between gap-4 m-8 text-2xl text-white">
-            <FaLinkedin />
-            <FaGithub />
-            <FaSquareXTwitter />
+            <a href="https://www.linkedin.com/in/sarvagya-patel-b0575918b/"><FaLinkedin className="hover:cursor-pointer"/></a>
+            <a href="https://github.com/sarvagyapatel"><FaGithub className="hover:cursor-pointer"/></a>
+            <a href="https://x.com/Sarvagya__Patel"><FaSquareXTwitter className="hover:cursor-pointer"/></a>
         </div>
     </nav>
   )
